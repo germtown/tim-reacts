a0cfb4cf32af70daceaa8bbdd5fa0d434048c47f
germtown <timothyebinger@gmail.com>
2020-08-30 15:23:49 -0700

    initial app commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I love you
+        </p>
+        <a
+          className="App-link"
+          href="timothyebinger.bandcamp.com"
+        >
+          Bandcamp!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
